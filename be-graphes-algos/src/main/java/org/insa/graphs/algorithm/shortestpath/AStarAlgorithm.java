4f1d5b9821ae29581eb8eab2217e80b95197675f
yacine180496 <benchehi@etud.insa-toulouse.fr>
2020-05-04 02:12:32 +0200

    AStarAlgorithm

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -1,9 +1,19 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.model.Node;
+
 public class AStarAlgorithm extends DijkstraAlgorithm {
 
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
     }
+    
+    @Override
+    public Label newLabel(Node node,ShortestPathData data) {
+    	return new LabelStar(node,data);
+    }
+    
+    
+   
 
 }
